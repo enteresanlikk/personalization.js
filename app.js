e028ef022e13007eb64899c387f49d31ede2ab49
Bilal <zeki1198@gmail.com>
2022-02-11 18:48:55 +0300

    Added checkbox auto select.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const cookieKey = 'personalization';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const cookie = new Cookie();
 
   const personalization = new Personalization(data);
@@ -11,15 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     selectedTags = [];
   }
+
+  selectedTags.map(t => {
+    const element = document.querySelector(`.radio-btn[data-type="${t.type}"][data-tag="${t.tag}"]`);
+    
+    if(element) element.setAttribute('checked', 'checked');
+  });
+
   personalization.run(selectedTags);
 
-  document.querySelectorAll(".radio-btn").forEach((btn) => {
-    btn.addEventListener("change", () => {
+  document.querySelectorAll('.radio-btn').forEach((btn) => {
+    btn.addEventListener('change', () => {
       let arr = [];
-      document.querySelectorAll(".radio-btn:checked").forEach((item) => {
+      document.querySelectorAll('.radio-btn:checked').forEach((item) => {
         arr.push({
-          type: item.getAttribute("data-type"),
-          tag: item.getAttribute("data-tag"),
+          type: item.getAttribute('data-type'),
+          tag: item.getAttribute('data-tag'),
         });
       });
 
